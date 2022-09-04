b82a9c2c0ae8d2dcdbc7299a291dd23a8dd93556
Des117 <50516919+Des117H@users.noreply.github.com>
2022-09-04 16:41:11 +0700

    Utilize WordJSON

--- BrotherDictionary/Word/Word.swift
+++ BrotherDictionary/Word/Word.swift
@@ -13,17 +13,19 @@
 import Foundation
 
 struct Word: Codable, Identifiable {
-	let id = UUID()
-	var word: String
-	var definitions: [String: [String]]
-	var sourceUrl: String
+	var id = UUID()
+	let word: String
+	let pronunciation: String
+	let definitions: [String: [String]]
+	let audioUrl: String
 	var userEdit: String?
-	var wordForms: [String]
+	let wordForms: [String]
 	
-	init(word: String, definiton: [String: [String]], sourceURL: String, wordForms: [String]) {
+	init(word: String, pronunciation: String, definiton: [String: [String]], audioUrl: String, wordForms: [String]) {
 		self.word = word
+		self.pronunciation = pronunciation
 		self.definitions = definiton
-		self.sourceUrl = sourceURL
+		self.audioUrl = audioUrl
 		self.wordForms = wordForms
 	}
 }
@@ -33,6 +35,8 @@ func ConvertJSONtoWord(word: WordJSON)-> Word {
 	var defTemp: String
 	var meanTemp: [String] = []
 	var wordForms: [String] = []
+	var pronunciation = ""
+	var audioUrl = ""
 	
 	for def in word[0].meanings {
 		defTemp = def.partOfSpeech
@@ -44,17 +48,23 @@ func ConvertJSONtoWord(word: WordJSON)-> Word {
 		definitions[defTemp] = meanTemp
 	}
 	
-	return Word(word: word[0].word, definiton: definitions, sourceURL: "", wordForms: wordForms)
+	for phonetic in word[0].phonetics {
+		if let pronun = phonetic.text {
+			pronunciation = pronun
+			audioUrl = phonetic.audio
+			break
+		}
+	}
+	
+	return Word(word: word[0].word, pronunciation: pronunciation,
+				definiton: definitions, audioUrl: audioUrl, wordForms: wordForms)
 }
 
 func decoceJsonFileToStruct(wordSearch: String) -> WordJSON {
 	if let url = URL(string: "https://api.dictionaryapi.dev/api/v2/entries/en/\(wordSearch)") {
 		if let data = try? Data(contentsOf: url) {
 			do {
-				let decoded = try JSONDecoder().decode(WordJSON.self, from: data)
-				print(decoded)
-
-				return decoded
+				return try JSONDecoder().decode(WordJSON.self, from: data)
 			} catch let err {
 				fatalError("Failed to encode JSON: \(err)")
 			}
