b3d2b160d2c405ea2577849ada53dd065be9524b
Des117 <50516919+Des117H@users.noreply.github.com>
2022-09-04 14:05:59 +0700

    Initial Commit

---
+++ BrotherDictionary/BrotherDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrotherDictionaryApp.swift
+//  BrotherDictionary
+//
+//  Created by Dinh Tu on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrotherDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
